a178ed6ab80f672a446e5551afd40a04d6915dff
sorabh86 <ssorabh.ssharma@gmail.com>
2023-10-05 15:52:59 +0530

    added models and update the designs of components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes:Recipe[] = [
+    new Recipe("First Recipe", "This is my first receipt", "https://th.bing.com/th/id/OIP.6X1F4ij9TbWz17aC8zhAUAHaHa?pid=ImgDet&rs=1"),
+    new Recipe("First Recipe", "This is my first receipt", "https://th.bing.com/th/id/OIP.6X1F4ij9TbWz17aC8zhAUAHaHa?pid=ImgDet&rs=1"),
+  ];
 
+  constructor(){}
+
+  ngOnInit() {
+
+  }
 }
