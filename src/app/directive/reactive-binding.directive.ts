e3514d959fe52ca83c71705e155d9571982b80f0
sorabh86 <ssorabh.ssharma@gmail.com>
2023-10-07 21:10:01 +0530

    reactive binding directive created

--- src/app/directive/reactive-binding.directive.ts
+++ src/app/directive/reactive-binding.directive.ts
@@ -3,24 +3,27 @@ import { Directive,
   Renderer2,
   HostListener,
   HostBinding,
-  OnInit
+  OnInit,
+  Input
 } from '@angular/core';
 
 @Directive({
   selector: '[appReactiveBinding]'
 })
 export class ReactiveBindingDirective implements OnInit {
-  defaultColor:string = 'lightgreen';
-  hoverColor:string = 'yellow';
+  @Input() defaultColor:string = 'lightgreen';
+  @Input() hoverColor:string = 'yellow';
   @HostBinding('style.backgroundColor') backgroundColor:string;
 
-  constructor(private eleRef:ElementRef, private renderer:Renderer2) { }
+  constructor(private eleRef:ElementRef) { }
   ngOnInit(): void {
       this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseenter(e:Event) {
     this.backgroundColor = this.hoverColor;
+    console.log('hello');
+    
   }
 
   @HostListener('mouseleave') mouseleave(e:Event) {
