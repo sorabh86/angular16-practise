c335853229a62a70e18ea96482401ac6fd3b038f
sorabh86 <ssorabh.ssharma@gmail.com>
2023-10-10 19:46:23 +0530

    getting params from route :id

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,29 +1,36 @@
-import { Component,OnInit } from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user = {
-    id:1,
-    name:"sorabh"
+    id: 1,
+    name: 'sorabh',
   };
+  paramsSubscription: Subscription;
 
-  constructor(private route:ActivatedRoute){}
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-      this.user = {
-        id: this.route.snapshot.params['id'],
-        name: 'Unknown'
-      };
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: 'Unknown',
+    };
 
-      // fix not updating data when redirecting to same component but data is not updating
-      this.route.params.subscribe((params:Params) => {
-        this.user.id = params['id'];
-        this.user.name = params['name'];
-      });
+    // fix not updating data when redirecting to same component but data is not updating
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
+      this.user.id = params['id'];
+      this.user.name = params['name'];
+    });
+  }
+
+  // if you add your own observers remove it.
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
   }
 }
