a0e7d389213aea3d53975a12dd455836fe1e9322
sorabh86 <ssorabh.ssharma@gmail.com>
2023-10-12 19:58:52 +0530

    create routing in module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,23 +9,11 @@ import { ServerComponent } from './servers/server/server.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './users/user/user.component';
-import { Routes, RouterModule } from '@angular/router';
 import { ServersService } from './servers/servers.service';
 import { UserService } from './users/users.service';
 import { PageNotFoundComponent } from './errors/page-not-found/page-not-found.component';
-
-const appRoutes:Routes = [
-  {path:'', component:HomeComponent},
-  {path:'users', component:UsersComponent, children:[
-    {path:':id', component:UserComponent},
-  ]},
-  {path:'servers', component:ServersComponent, children:[
-    {path:':id', component:ServerComponent},
-    {path:':id/edit', component:EditServerComponent},
-  ]},
-  {path:'not-found', component:PageNotFoundComponent},
-  {path:'**', redirectTo:'not-found'},
-];
+import { AppRoutingModule } from './app-rouding.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -39,7 +28,9 @@ const appRoutes:Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [ServersService, UserService],
   bootstrap: [AppComponent]
