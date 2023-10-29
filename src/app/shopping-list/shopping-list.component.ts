c9f308a85a974181643a18164d23aaa955ba605a
sorabh86 <ssorabh.ssharma@gmail.com>
2023-10-29 17:21:08 +0530

    fix more unwanted code, and use Subject rxjs

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients:Ingredient[];
+  private changeSubscription:Subscription;
 
   constructor(private shoppingListService:ShoppingListService){}
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientChanged.subscribe(
+    this.changeSubscription = this.shoppingListService.ingredientChanged.subscribe(
       d => this.ingredients = d
     )
   }
 
   onIngredientAdded(ingredient:Ingredient) {
     this.ingredients.push(ingredient);
+  }
+
+  ngOnDestroy(): void {
+      this.changeSubscription.unsubscribe();
   }
 }
