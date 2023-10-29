f0b49b60e1121d87c3a11acd36741839ca0d2849
sorabh86 <ssorabh.ssharma@gmail.com>
2023-10-29 17:02:36 +0530

    added obervable subject from rxjs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -25,6 +25,6 @@ export class UserComponent implements OnInit {
 
   onActivate() {
     // this.userService.activatedEmitter.emit(true);
-    this.userService.activatedEmitter.emit(true);
+    this.userService.activatedEmitter.next(true);
   }
 }
