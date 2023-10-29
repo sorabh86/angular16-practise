a9f14786499d2c569043c402c097c37d7249ed2f
sorabh86 <ssorabh.ssharma@gmail.com>
2023-10-29 17:08:05 +0530

    fix memory leaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserService } from './user.service';
+import {Subscription} from 'rxjs';
 
 export type User = {
   title:string;
@@ -11,20 +12,25 @@ export type User = {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   users:User[] = [
     {title:"User 1", body:"User 1 is great, welcome to our land."},
     {title:"User 2", body:"User 2 welcome, we are waiting for you."},
   ];
   isActivated = false;
+  subscription:Subscription;
 
   constructor(
     private userService:UserService
   ){}
 
   ngOnInit(): void {
-    this.userService.activatedEmitter.subscribe(bool=>{
+    this.subscription = this.userService.activatedEmitter.subscribe(bool=>{
       this.isActivated=bool;
     });   
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe();
   }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -4,5 +4,7 @@ import {Subject} from 'rxjs';
 @Injectable({providedIn:'root'})
 export class UserService {
     // activatedEmitter = new EventEmitter<boolean>();
+
+    // Subject is special type of Observable which can be called method next() from outside
     activatedEmitter = new Subject<boolean>();
 }
